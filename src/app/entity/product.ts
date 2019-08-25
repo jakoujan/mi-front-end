bea41bfe10085464d102583a9611c6b59bc66932
Edgar Rangel <jakoujan@gmail.com>
2019-08-25 02:05:55 -0500

    services

---
+++ src/app/entity/product.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+    id: number;
+    capacity: string;
+    code: string;
+    color: string;
+    description: string;
+    imageUrl: string;
+    price: number;
+    sku: string;
+}
